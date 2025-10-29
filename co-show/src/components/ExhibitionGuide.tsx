56e61264c9b42cd431be019f3898f3dbbb8074c2
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-10-30 05:44:23 +0900

    전시회 안내 페이지 개발/ asset 일부 반영

---
+++ co-show/src/components/ExhibitionGuide.tsx
@@ -0,0 +1,71 @@
+// src/components/ExhibitionGuide.jsx
+import "../styles/ExhibitionGuide.css";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+
+const Section = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  box-sizing: border-box;
+
+  /* 배경 */
+  background-image: url(
+    "src/assets/ExhibitionGuide/전시장 안내.svg");
+  background-size: cover;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+
+  display: flex;
+  justify-content: flex-end; /* 카드 오른쪽으로 */
+  align-items: center;
+  padding: 80px 10% 120px 10%; /* 아래 여백을 좀 더 줘서 balloon이 안 잘림 */
+  color: #1a1a1a;
+  font-family: "Pretendard", system-ui, -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif;
+`;
+
+
+
+const ExhibitionGuide = () => {
+  const navigate = useNavigate(); // ✅ 페이지 이동 훅
+
+  return (
+    <Section>
+     {/* 🔵 전체 지도 보기 */}
+      <button className="guide-btn blue" onClick={() => navigate("/map")}>
+        <div className="icon-box">
+          <img src="src/assets/ExhibitionGuide/free-icon-search-3434958 2.png" alt="전체 지도 아이콘" />
+        </div>
+        <h3>전체 지도 보기</h3>
+        <p>전시장의 지도를 확인해 보세요</p>
+      </button>
+      <img
+        src="src/assets/ExhibitionGuide/free-icon-blue-tit-9254765 2.png"  /* 파일 경로에 맞게 변경 */
+        alt="파란 새"
+        className="blue-bird"
+      />
+
+
+      {/* 🔴 빠른 길찾기 */}
+      <button className="guide-btn red" onClick={() => navigate("/search")}>
+        <div className="icon-box">
+          <img src="src/assets/ExhibitionGuide/Group 32.png" alt="길찾기 아이콘" />
+        </div>
+        <h3>빠른 길찾기</h3>
+        <p>원하는 위치까지 안내해 드릴게요</p>
+      </button>
+
+      {/* 🟣 추천 장소 */}
+      <button className="guide-btn purple" onClick={() => navigate("/recommend")}>
+        <div className="icon-box">
+          <img src="src/assets/ExhibitionGuide/free-icon-travel-guide-15773474 2.png" alt="추천 장소 아이콘" />
+        </div>
+        <h3>추천 장소</h3>
+        <p>추천을 받고, 작품을 감상해 보세요</p>
+      </button>
+    </Section>
+  );
+};
+
+export default ExhibitionGuide;
