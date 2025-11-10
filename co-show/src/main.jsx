efd3c8cdc39f64ce848db70ecbefb1f9145fe36f
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-10 09:27:34 +0900

    불필요 파일 삭제/ 오답 화면 구성

--- co-show/src/main.jsx
+++ co-show/src/main.jsx
@@ -7,7 +7,6 @@ import App from "./App.jsx";
 import Guide from "./pages/Guide.jsx";
 import Events from "./pages/Events.jsx";
 import Photo from "./pages/Photo.jsx";
-import MapPage from "./pages/MapPage.jsx";
 import ZoneDetailPage from "./pages/ZoneDetailPage.jsx";
 import QuickSearch from "./pages/QuickSearch.jsx";
 import RecommendList from "./pages/RecommendList.jsx";
@@ -17,7 +16,6 @@ import Inquiry from "./pages/Inquiry.jsx";
 import EventComplete from "./pages/EventComplete.jsx";   
 import EventPhone from "./pages/EventPhone.jsx";
 import EventFinish from "./pages/EventFinish.jsx";
-import { HashRouter } from "react-router-dom";
 import Quiz from "./pages/Quiz.jsx";
 import PhotoStart from "./pages/PhotoStart.jsx";
 import TrainNav from "./pages/TrainNav.jsx";
@@ -29,16 +27,13 @@ import QuizQuestion from "./pages/QuizQuestion.jsx";
 import QuizResult from "./pages/QuizResult.jsx";
 import QuizWrong from "./pages/QuizWrong.jsx";
 import QuizIntro from "./pages/QuizIntro.jsx";
-import TypeStart from "./pages/type/TypeQuestion.tsx";
 import Q1 from "./pages/type/Q1";
 import Q2Make from "./pages/type/Q2Make";
 import Q2Read from "./pages/type/Q2Read";
 import TypeResult from "./pages/type/TypeResult";
 import TypeTest from "./pages/TypeTest.jsx";
 import QuizCorrect from "./pages/QuizCorrect.jsx";
-import QuizQ1 from "./pages/QuizQ1.jsx";
-import Quiz1Result from "./pages/Quiz1Result.jsx";
-import Quiz1Wrong from "./pages/Quiz1Wrong.jsx";
+
 import TypeQuestion from "./pages/type/TypeQuestion.tsx";
 
 const router = createHashRouter([
