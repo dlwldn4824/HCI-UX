64f906502b7a11f93512af1d293a716579bf021f
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-14 23:51:29 +0900

    Temp save before rebase

--- co-show/vite.config.js
+++ co-show/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  // 레포 이름으로 변경 (예: repo 이름이 'co-show' 라면 "/co-show/")
+  base: "/HCI-UX/",
+});
