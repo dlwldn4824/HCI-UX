843371f7f795c0fd8b58e377d34d1e828b1416a1
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-15 00:04:17 +0900

    배포용 에러 수정

--- co-show/src/pages/QuizResult.jsx
+++ co-show/src/pages/QuizResult.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import "../styles/quiz.css";
 
 export default function QuizResult() {
   const { qid } = useParams();
--- co-show/vite.config.js
+++ co-show/vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // 레포 이름으로 변경 (예: repo 이름이 'co-show' 라면 "/co-show/")
-  base: "/HCI-UX/",
 });
