f189d120f6c51f11b7a3221506c7c2c8c1f198ff
Yooaknow <dbdkfma1228@naver.com>
2025-11-17 00:54:19 +0900

    Inquiry 페이지 버튼 위치 및 레이아웃 수정

--- co-show/src/pages/Inquiry.jsx
+++ co-show/src/pages/Inquiry.jsx
@@ -2,32 +2,33 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/inquiry.css";
 
-/**
- * Inquiry 페이지
- * - 배경과 버튼 이미지는 CSS에서 직접 지정
- * - JSX에서는 위치 및 구조만 정의
- */
 export default function Inquiry() {
   const navigate = useNavigate();
 
-   useEffect(() => {
+  useEffect(() => {
     document.body.classList.add("inquiry-route");
     return () => document.body.classList.remove("inquiry-route");
   }, []);
 
   return (
     <div className="inquiry-canvas">
-      {/* 직원 호출 버튼 */}
-      <button
-        className="inquiry-btn btn-employee"
-        onClick={() => navigate("/inquiry/employee")} // 직원 호출 페이지로 이동
-      />
 
-      {/* 문의만 하기 버튼 */}
-      <button
-        className="inquiry-btn btn-userinquiry"
-        onClick={() => navigate("/inquiry/justinquiry")} // 문의만 하기 페이지로 이동
-      />
+      <img
+  src="/src/assets/inquiry/직원도움.svg"
+  alt="직원 도움 받기"
+  className="inquiry-btn-img"
+  style={{ top: "700px", left: "130px" }}  
+  onClick={() => navigate("/inquiry/employee")}
+/>
+
+<img
+  src="/src/assets/inquiry/문의.svg"
+  alt="문의 남기기"
+  className="inquiry-btn-img"
+  style={{ top: "900px", left: "130px" }}  
+  onClick={() => navigate("/inquiry/justinquiry")}
+/>
+
     </div>
   );
 }
