a0800f7f755907750a069c23276f4e3e3e005d76
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-19 13:38:56 +0900

    유형테스트 잔상 남는 현상 수정 , 문의하기 가장자리 이미지 잘리는 현상 수정

--- co-show/src/pages/TypeTest.jsx
+++ co-show/src/pages/TypeTest.jsx
@@ -6,8 +6,8 @@ export default function TypeTest() {
   const nav = useNavigate();
 
   return (
-    <main className="type-start-page" /* 데모용 배경색 */ >
-      <div className="type-start-card">
+    <main className="type-start" /* 데모용 배경색 */ >
+      <div className="type-start-page">
         <button
           className="type-start-btn"
           onClick={() => nav("/type/q1")}
