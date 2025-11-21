7b170370ec8d17e67441ef81b5766367873f82c8
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-22 03:35:03 +0900

    설문페이지 수정

--- co-show/src/pages/Review.jsx
+++ co-show/src/pages/Review.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import bgImg from "../assets/review/설문배경.png";
-import qrImg from "../assets/review/실험_사후_설문지-1024.svg"; // ← QR 이미지 경로로 교체
+import bgImg from "../assets/review/설문_배경.png";
 
 export default function Review() {
   return (
@@ -17,18 +16,6 @@ export default function Review() {
         fontFamily: "nanumRound",
       }}
     >
-      {/* QR 이미지 */}
-      <img
-        src={qrImg}
-        alt="QR 코드"
-        style={{
-          position: "absolute",
-          left: "950px",
-          top:"550px",
-          width: "600px",     // 원하는 크기로 조절
-          height: "600px",
-        }}
-      />
     </main>
   );
 }
