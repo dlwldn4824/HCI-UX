0d39e13e76215f171c6ce0e16b449b26a7808770
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-23 02:46:19 +0900

    temi guide css 수정

--- co-show/src/pages/TemiGuide.jsx
+++ co-show/src/pages/TemiGuide.jsx
@@ -64,7 +64,7 @@ export default function TemiGuide() {
     >
       <div style={{ 
         position:"absolute",
-        top:"130px", left:"200px",
+        top:"130px", left:"355px",
         padding: "40px 60px",
         borderRadius: "20px",
         textAlign: "center"
