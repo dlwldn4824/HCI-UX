2f80529a72c94cd0a78b5637ad6d23068249366b
shinhyunwooo <freddyshin@naver.com>
2025-11-22 16:44:42 +0900

    안드로이드 스튜디오 코드 수정

--- co-show/android/app/src/main/java/com/example/coshowsample/MainActivity.java
+++ co-show/android/app/src/main/java/com/example/coshowsample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.coshowsample;
 
 import android.os.Bundle;
+
+import com.example.coshowsample.temi.TemiCallPlugin;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends BridgeActivity {
         // Temi SDK가 필요하면 아래 주석을 해제하고 sdk-debug.aar 파일을 libs 폴더에 추가하세요
         // import com.example.coshowsample.temi.TemiCallPlugin;
         // registerPlugin(TemiCallPlugin.class);
+        registerPlugin(TemiCallPlugin.class);
     }
 }
 
