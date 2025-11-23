1ea504ae1954a7144e23f8cf0e9acf527d4a269b
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-11-24 02:16:48 +0900

    네비게이션 바 수정

--- co-show/src/layout/RootLayout.jsx
+++ co-show/src/layout/RootLayout.jsx
@@ -8,8 +8,8 @@ export default function RootLayout() {
         display:"flex", alignItems:"center", justifyContent:"space-between",
         padding:"16px 20px"
       }}>
-        <div style={{ fontWeight: 800 ,fontSize: "25px"}}>Temi-Tell-Me</div>
-        <nav style={{ display:"flex", gap:50 }}>
+        <div style={{ fontWeight: 800 ,fontSize: "40px"}}>Temi-Tell-Me</div>
+        <nav style={{ display:"flex", gap:30 }}>
           {[
             ["홈", "/"],
             ["전시장 안내", "/guide"],
@@ -23,16 +23,42 @@ export default function RootLayout() {
             <NavLink
               key={to}
               to={to}
-              style={({ isActive }) => ({
-                padding:"8px 20px",
-                borderRadius:8,
-                background: isActive ? "rgba(0,0,0,0.06)" : "transparent",
-                fontWeight:1600,
-                fontSize: "25px",   // ⭐ 네비게이션 글씨 전체 크기
-              })}
+              style={({ isActive }) => {
+                const baseStyle = {
+                  padding: "8px 20px",
+                  borderRadius: 8,
+                  background: isActive ? "rgba(0,0,0,0.06)" : "transparent",
+                  fontWeight: 1600,
+                  fontSize: "25px",
+                  transition: "0.25s",
+                };
+
+                if (label === "평가 남기기") {
+                  return {
+                    ...baseStyle,
+                    color: "#ff4e4e",
+                    background: isActive
+                      ? "rgba(255,0,0,0.1)"
+                      : "rgba(255,200,200,0.25)",
+                    border: "2px solid rgba(255,0,0,0.35)",
+                    borderRadius: "12px",
+                    padding: "8px 26px",
+                    fontWeight: 900,
+                  };
+                }
+
+                  return {
+                    ...baseStyle,
+                    fontFamily: "nanumRound",
+                    fontWeight: 500,
+                    fontSize: "28px",
+                    
+                  };
+              }}
             >
               {label}
             </NavLink>
+
           ))}
         </nav>
       </header>
--- co-show/src/pages/Home.jsx
+++ co-show/src/pages/Home.jsx
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <div className="home-wrapper">
       <CardNav />
+      <div className="moving-footer">
+        <div className="moving-text">
+          본 프로젝트는 we-meet 수업의 프로젝트 결과물로 제작되었습니다.
+        </div>
+      </div>
+
     </div>
   );
 }
