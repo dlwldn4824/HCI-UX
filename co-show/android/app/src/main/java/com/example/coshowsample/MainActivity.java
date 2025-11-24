01215dd737ef49c91b6d0e31245c13ee875d2e20
bhw119 <bhw119@gmail.com>
2025-11-24 17:38:48 +0900

    앱 연결 완료

--- co-show/android/app/src/main/java/com/example/coshowsample/MainActivity.java
+++ co-show/android/app/src/main/java/com/example/coshowsample/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.coshowsample;
 
 import android.os.Bundle;
 
-import com.example.coshowsample.temi.TemiCallPlugin;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -13,7 +13,16 @@ public class MainActivity extends BridgeActivity {
         // Temi SDK가 필요하면 아래 주석을 해제하고 sdk-debug.aar 파일을 libs 폴더에 추가하세요
         // import com.example.coshowsample.temi.TemiCallPlugin;
         // registerPlugin(TemiCallPlugin.class);
-        registerPlugin(TemiCallPlugin.class);
+        
+        // SDK가 없으면 TemiCallPlugin 등록 건너뜀
+        try {
+            Class<?> temiPluginClass = Class.forName("com.example.coshowsample.temi.TemiCallPlugin");
+            @SuppressWarnings("unchecked")
+            Class<? extends Plugin> pluginClass = (Class<? extends Plugin>) temiPluginClass;
+            registerPlugin(pluginClass);
+        } catch (ClassNotFoundException e) {
+            // Temi SDK가 없으면 무시
+        }
     }
 }
 
